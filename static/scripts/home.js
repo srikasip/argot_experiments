237427689ce97ac6eb479112cc4d8a53e389f48c
srikasip <srikasip@gmail.com>
2017-03-23 16:28:09 -0400

    Added analytics to the video, links, down arrow, a backside to each team member

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,6 +1,77 @@
 $(document).ready(function(){
+
+  HandlePlayer();
+
   $("#signupBtn").click(function(){SubmitClicked();});
+
+  $(".teamer-group").click(function(e){
+    $(".teamer-writeup").css("display", "none");
+    $myWriteup = $(this).find(".teamer-writeup");
+    //$($myWriteup).css("display", "block");
+    $($myWriteup).slideDown("slow");
+
+    e.stopPropagation();
+  });
+
+  $("#team-block").click(function(){
+    $(".teamer-writeup").css("display", "none");
+  });
+
+  CheckScroller();
+
+  $(window).scroll(function () { CheckScroller(); } );
+  $(window).resize(function() { CheckScroller(); } );
+
 });
+
+function CheckScroller()
+{
+  totalHeight = $(document).height() - $(window).height() - $(".footer").outerHeight();
+  scrollHeight = $(window).scrollTop();
+
+  if(scrollHeight < totalHeight)
+  {
+    $("#downarrow").css("display", "block");
+  }
+  else
+  {
+    $("#downarrow").css("display", "none");
+  }
+}
+
+function HandlePlayer()
+{
+  var iframe = document.querySelector('iframe');
+  var player = new Vimeo.Player(iframe);
+
+  player.on('play', function() {
+    ga('send', {
+          hitType: 'event',
+          eventCategory: 'Videos',
+          eventAction: 'Play',
+          eventLabel: 'Started Jane - Data Science'
+      });
+  });
+  player.on("pause", function(){
+    ga('send', {
+          hitType: 'event',
+          eventCategory: 'Videos',
+          eventAction: 'Pause',
+          eventLabel: 'Paused Jane - Data Science'
+      });
+  });
+  player.on("ended", function(){
+    ga('send', {
+          hitType: 'event',
+          eventCategory: 'Videos',
+          eventAction: 'Play',
+          eventLabel: 'Finished Jane - Data Science'
+      });
+  });
+  // player.getVideoTitle().then(function(title) {
+  //     console.log('title:', title);
+  // });
+}
 
 function SubmitClicked()
 {
@@ -11,6 +82,13 @@ function SubmitClicked()
   reason = $("#whyForm").val();
   if(ValidateEmail(email))
   {
+    ga('send', {
+          hitType: 'event',
+          eventCategory: 'Signup',
+          eventAction: 'SignedUp',
+          eventLabel: 'Signup - Data Science'
+    });
+
     dataSource = {"Email": email, "FirstName": name, "Reason": reason};
     $("#form-overlay").css("display", "block");
     $.ajax({
